0fa24561312611231f0e171ccae7295624c986c9
Rahul Rudra <rudra.rahul71@gmail.com>
2026-09-01 13:52:45 -0400

    random..

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.busy_bee"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "28.2.13676358"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
